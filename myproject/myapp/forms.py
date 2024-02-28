bdc35025bdb34b322ea20733d25f04cb11170022
RolaMmss <rola.el-moallem@hotmail.fr>
2024-02-28 16:59:49 +0100

    add form and model for the api service

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from . import models
 
 
 class SignupForm(UserCreationForm):
@@ -12,4 +13,16 @@ class SignupForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
-    
+
+
+class ApiForm(forms.ModelForm):
+    class Meta:
+        model = models.ApiModel
+        fields = "__all__"
+        labels = {
+            "url": "Entrez l'url de votre image",
+            # "num_keywords": "Entrez le nombre max de mots-clés"
+        }
+        # widgets = {
+        #     "api_choices":CheckboxSelectMultiple()
+        # }   
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ApiModel(models.Model):
+    url = models.URLField(
+        max_length=100,
+        blank=False,
+        null=True,
+    )
