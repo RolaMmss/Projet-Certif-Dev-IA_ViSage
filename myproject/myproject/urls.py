7b3a4b65aa75d86d221ea367834497fb02a6a8b1
RolaMmss <rola.el-moallem@hotmail.fr>
2024-04-22 12:22:02 +0200

    urls.py mod

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('login/',views.login_user, name='login'),
     path('logout/',views.logout_user, name='logout'),
-    # path('api/',views.api, name='api'),
     path('api/', include('myapp.urls'))
 ]
