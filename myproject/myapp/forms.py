6358b760c918899901a71174eb7bac278e73142b
RolaMmss <rola.el-moallem@hotmail.fr>
2024-05-21 16:18:16 +0200

    modify ApiForm to save images/predictions

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from . import models
+from .models import ImagePrediction
 
 
 class SignupForm(UserCreationForm):
@@ -14,16 +15,23 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
 
+#In case we don't want to save the images in a database
 
-class ApiForm(forms.ModelForm):
-    class Meta:
-        model = models.ApiModel
-        fields = "__all__"
-        labels = {
-            "url": "Entrez l'url de votre image",
-            # "num_keywords": "Entrez le nombre max de mots-clés"
-        }
+# class ApiForm(forms.ModelForm):
+#     class Meta:
+#         model = models.ApiModel
+#         fields = "__all__"
+#         labels = {
+#             "url": "Entrez l'url de votre image",
+#             # "num_keywords": "Entrez le nombre max de mots-clés"
+#         }
         
-        # widgets = {
-        #     "api_choices":CheckboxSelectMultiple()
-        # }   
+        
+# # Ensure that the ApiForm points to the correct model.
+# class ApiForm(forms.ModelForm):
+#     class Meta:
+#         model = ImagePrediction
+#         fields = ['image_url']
+#         labels = {
+#             'image_url': "Entrez l'URL de votre image",
+#         }
