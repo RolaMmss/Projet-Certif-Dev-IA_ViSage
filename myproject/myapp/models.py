522825e806bdb7185d3cdac333bcec8310eececb
RolaMmss <rola.el-moallem@hotmail.fr>
2024-05-21 16:18:59 +0200

    add a model ImagePrediction for the image/prediction

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# In case we don't want to save the images and their predictions
+# class ApiModel(models.Model):
+#     url = models.URLField(
+#         max_length=100,
+#         blank=False,
+#         null=True,
+#     )
+    
+# Add a model to store the image information and predictions
+class ImagePrediction(models.Model):
+    image_url = models.URLField()
+    prediction_data = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-class ApiModel(models.Model):
-    url = models.URLField(
-        max_length=100,
-        blank=False,
-        null=True,
-    )
+    def __str__(self):
+        return f"Image URL: {self.image_url} - Predictions: {self.prediction_data}"
+
+# After adding the model, run the migrations to create the table in your database.
+# python manage.py makemigrations
+# python manage.py migrate
